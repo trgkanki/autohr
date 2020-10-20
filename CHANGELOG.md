7b60846cd82be3e1dea2c9e06ddf05915df4ad2f
Hyun Woo Park <phu54321@naver.com>
2020-10-20 14:31:45 +0900

    :bookmark: v20.10.20i59
    Initial release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 [comment]: # (DO NOT MODIFY. new changelog goes here)
 
+## 20.10.20i59 (2020-10-20)
+
+Initial release
+
 ## 20.10.7i141 (2020-10-07)
 
 Initial commit
--- src/__init__.py
+++ src/__init__.py
@@ -15,7 +15,7 @@
 
 # -*- coding: utf-8 -*-
 #
-# autohr v20.10.7i141
+# autohr v20.10.20i59
 #
 # Copyright: trgk (phu54321@naver.com)
 # License: GNU AGPL, version 3 or later;
